c202d69f0ea60324083c5cd385dc03a514250626
Manoj <manojd@rgmobiles.com>
2020-09-19 03:03:16 +0530

    fetch get api

---
+++ src/fectchapi.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class Toggle extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {isToggleOn: true,
+        date:[],
+     
+    }
+
+    
+  
+ 
+    }
+  
+    handleClick=() =>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => 
+        this.setState({
+            date:data
+          })
+          )
+    }
+  
+    render() {
+        console.log("888ds",this.state.date)
+      return (
+        <div className="container">
+          <button onClick={this.handleClick}>result</button>
+          <div className="table-responsive">
+            <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <th>id</th>
+                  <th>name</th>
+                  <th>username</th>
+                  <th>email</th>
+                 <th>address</th>
+                 <th>phone</th>
+                  
+                </tr>
+              </thead>
+              <tbody>
+              {this.state.date.map((item) => (
+     
+  
+                <tr>
+
+              <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.username}</td>
+              <td>{item.email}</td>
+              <td>{item.address.geo.lat}<br></br>
+              {item.address.street}<br></br>
+              
+              {item.address.suite}
+              
+              
+              </td>
+              <td>{item.phone}</td>
+                 
+</tr>
+  ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  export default Toggle;
